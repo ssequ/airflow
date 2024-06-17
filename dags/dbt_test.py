bab671a8b066d24f1bf7ab7a56aff7208d31baf6
ssequ <163583515+ssequ@users.noreply.github.com>
2024-06-17 08:10:16 +0200

    Update dbt_test.py

--- dags/dbt_test.py
+++ dags/dbt_test.py
@@ -40,7 +40,7 @@ dag = DAG(
 
 dbt_task = BashOperator(
   task_id="test-task",
-  bash_command="dbt run -m --project-dir /tmp/dags --profiles-dir /tmp/dags/orchestration; sleep 3600",
+  bash_command="dbt run -m --project-dir /opt/airflow/dbt --profiles-dir /opt/airflow/dbt/orchestration; sleep 3600",
   dag=dag,
   trigger_rule='all_done',
   executor_config=dbt_bash_executor_config
